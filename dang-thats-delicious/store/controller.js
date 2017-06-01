84518251e3079e1401ae9971f9a8e00fbcbfea59
Dan Denney <dd.subscriptions@gmail.com>
2017-06-01 08:22:43 -0400

    Output stores in a store page

--- dang-thats-delicious/store/controller.js
+++ dang-thats-delicious/store/controller.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   res.render('index');
 };
@@ -6,6 +9,15 @@ exports.addStore = (req, res) => {
   res.render('addEditStore', { title: 'Add Store' });
 };
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
+exports.createStore = async (req, res) => {
+  const store = await new Store(req.body).save();
+  await store.save();
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  console.log(stores);
+  res.render('stores', { title: 'Stores', stores });
 };
