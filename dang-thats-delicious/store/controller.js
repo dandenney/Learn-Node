36a58e892d64d967d2efe6a83a994265d524e2b3
Dan Denney <dd.subscriptions@gmail.com>
2017-06-02 08:51:58 -0400

    feat: Add edit/update to the store form

--- dang-thats-delicious/store/controller.js
+++ dang-thats-delicious/store/controller.js
@@ -6,7 +6,7 @@ exports.homePage = (req, res) => {
 };
 
 exports.addStore = (req, res) => {
-  res.render('addEditStore', { title: 'Add Store' });
+  res.render('addStore', { title: 'Add Store' });
 };
 
 exports.createStore = async (req, res) => {
@@ -20,4 +20,24 @@ exports.getStores = async (req, res) => {
   const stores = await Store.find();
   console.log(stores);
   res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id });
+
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  // Find and update
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true,
+    runValidators: true
+  }).exec();
+  req.flash(
+    'success',
+    `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.pug}">View Store -></a>`
+  );
+  res.redirect(`/stores/${store._id}/edit`);
+  // Redirect
 };
