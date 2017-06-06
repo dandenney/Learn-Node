27b8abd8bbea7b9dba3869e8714b6cae8d495630
Dan Denney <dd.subscriptions@gmail.com>
2017-06-06 07:52:30 -0400

    Add image upload functionality

--- dang-thats-delicious/store/controller.js
+++ dang-thats-delicious/store/controller.js
@@ -1,5 +1,19 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if (isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: "That filetype isn't allowed!" }, false);
+    }
+  }
+};
+const jimp = require('jimp');
+const uuid = require('uuid');
 
 exports.homePage = (req, res) => {
   res.render('index');
@@ -7,6 +21,21 @@ exports.homePage = (req, res) => {
 
 exports.addStore = (req, res) => {
   res.render('addStore', { title: 'Add Store' });
+};
+
+exports.upload = multer(multerOptions).single('photo');
+exports.resize = async (req, res, next) => {
+  // check for a new file
+  if (!req.file) {
+    next(); // skip to next middleware
+    return;
+  }
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  next();
 };
 
 exports.createStore = async (req, res) => {
@@ -29,6 +58,8 @@ exports.editStore = async (req, res) => {
 };
 
 exports.updateStore = async (req, res) => {
+  // set the location to be a points
+  req.body.location.type = 'Point';
   // Find and update
   const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true,
--- dang-thats-delicious/store/model.js
+++ dang-thats-delicious/store/model.js
@@ -33,7 +33,8 @@ const storeSchema = new mongoose.Schema({
       type: String,
       required: 'You must supply and address!'
     }
-  }
+  },
+  photo: String
 });
 
 storeSchema.pre('save', function(next) {
