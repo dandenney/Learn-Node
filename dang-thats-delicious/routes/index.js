82fba4a976a6a58c6f3e56eedfc0b3ae56469e14
dandenney <dd.subscriptions@gmail.com>
2017-07-24 22:16:08 -0400

    feat: Display stores on a map with custom data FTW!

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -50,9 +50,11 @@ router.post(
   authController.confirmedPasswords,
   catchErrors(authController.update)
 );
+router.get("/map", storeController.mapPage);
 
 /* API */
 
 router.get("/api/search", catchErrors(storeController.searchStores));
+router.get("/api/stores/near", catchErrors(storeController.mapStores));
 
 module.exports = router;
