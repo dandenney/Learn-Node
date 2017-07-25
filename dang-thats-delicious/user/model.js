dfbafcd05ef3871eb8395ea5560b7776f2d0c182
dandenney <dd.subscriptions@gmail.com>
2017-07-25 13:17:22 -0400

    feat: Add heart functionality to API and UI

--- dang-thats-delicious/user/model.js
+++ dang-thats-delicious/user/model.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 mongoose.Promise = global.Promise;
-const md5 = require('md5');
-const validator = require('validator');
-const mongodbErrorHandler = require('mongoose-mongodb-errors');
-const passportLocalMongoose = require('passport-local-mongoose');
+const md5 = require("md5");
+const validator = require("validator");
+const mongodbErrorHandler = require("mongoose-mongodb-errors");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const userSchema = new Schema({
   email: {
@@ -12,23 +12,24 @@ const userSchema = new Schema({
     unique: true,
     lowercase: true,
     trim: true,
-    validate: [validator.isEmail, 'Invalid Email Address'],
-    required: 'Plese provide an email address'
+    validate: [validator.isEmail, "Invalid Email Address"],
+    required: "Plese provide an email address"
   },
   name: {
     type: String,
-    required: 'Please provide a name',
+    required: "Please provide a name",
     trim: true
   },
   resetPasswordToken: String,
-  resetPasswordExpires: Date
+  resetPasswordExpires: Date,
+  hearts: [{ type: mongoose.Schema.ObjectId, ref: "Store" }]
 });
 
-userSchema.virtual('gravatar').get(function() {
+userSchema.virtual("gravatar").get(function() {
   const hash = md5(this.email);
   return `https://gravatar.com/avatar/${hash}?s=200`;
 });
-userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
+userSchema.plugin(passportLocalMongoose, { usernameField: "email" });
 userSchema.plugin(mongodbErrorHandler);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
